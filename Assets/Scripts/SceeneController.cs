f43542d14f0ae82dba1f294bf9adf20a6a936b48
Sounite <jajiso123@gmail.com>
2020-10-06 20:05:59 +0100

    My first commit

---
+++ Assets/Scripts/SceeneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceeneController : MonoBehaviour {
+
+	// Use this for initialization
+	public void changeLevel(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/playHorn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playHorn : MonoBehaviour {
+
+    AudioSource audio;
+
+	void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+	
+	public void Beep () {
+        audio.Play();
+	}
+}
